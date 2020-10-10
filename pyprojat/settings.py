12b80835d500da29411e5c564a95b23a15e9465a
brjatoba92 <brjatoba92@gmail.com>
2020-10-09 21:02:54 -0300

    Configurando secret key
    close #24

--- pyprojat/settings.py
+++ pyprojat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&mlbcdy5=-d$0$9yl2%*1knkx@i(bztqc@6wu0@ns^=o*3!r1('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
