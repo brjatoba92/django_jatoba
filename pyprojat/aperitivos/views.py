5827703ec802c9e0f21197d055deb42a4b6bde4f
brjatoba92 <brjatoba92@gmail.com>
2020-10-28 18:07:42 -0300

    Upload de novos videos
    close #105

--- pyprojat/aperitivos/views.py
+++ pyprojat/aperitivos/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 472243892}
+    videos = {
+        'instalacao-windows': {'titulo': 'Aula 1: Instalação Windows', 'vimeo_id': 473186781},
+        'instalacao-macos': {'titulo': 'Aula 2: Instalação MacOS', 'vimeo_id': 473187531},
+        'instalacao-ubuntu': {'titulo': 'Aula 3: Instalação Python Ubuntu', 'vimeo_id': 473187848},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
