3aac0581fa42b4c19cae4a3bf4d1e0ed751c819a
brjatoba92 <brjatoba92@gmail.com>
2020-11-04 20:41:43 -0300

    Iniciando app de modulos
    close #141

---
+++ pyprojat/modulos/facade.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from pyprojat.modulos.models import Modulo
+
+
+def listar_modulos_ordenados() -> List[Modulo]:
+    """
+    Lista módulos ordenados por titulos
+    :return:
+    """
+
+    return list(Modulo.objects.order_by('titulo').all())
