093bd57897a4bd6f6388e59c47d802238d4a1e93
brjatoba92 <brjatoba92@gmail.com>
2020-11-11 18:26:27 -0300

    Criado indice de modulos
    close #179

--- pyprojat/modulos/urls.py
+++ pyprojat/modulos/urls.py
@@ -5,5 +5,7 @@ from pyprojat.modulos import views
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
+    path('', views.indice, name='indice'),
     path('aulas/<slug:slug>', views.aula, name='aula'),
+
 ]
